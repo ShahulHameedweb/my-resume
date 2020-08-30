2c838b71c553a8219d3150c8a7b525c82cf61746
Shahul Hameed <shahul.hameed@thinktower.in>
2020-08-30 12:36:05 +0530

    Shahul update light or dark mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,40 @@
+import React, { useState, useEffect } from 'react'
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyles } from '../styles/global'
+import { lightTheme, darkTheme } from '../styles/themes'
 import '../styles/globals.css'
 
+
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [darkMode, setDarkMode] = useState()
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    const darkModeValue = localStorage.getItem('DARK_MODE')
+    // localStorage returns a string, not a boolean
+    setDarkMode(darkModeValue === 'true')
+    // Set mounted to `true` only after setting the `darkMode` state
+    setMounted(true)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('DARK_MODE', darkMode)
+  }, [darkMode]) // Run every time `darkMode` changes
+
+  if (!mounted) return <div />
+
+  return(
+  	    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+  	    <div>
+        <button className="themebtn" onClick={() => setDarkMode(!darkMode)}>
+          {darkMode ? 'Light Theme' : 'Dark Theme'}
+        </button>
+      </div>
+  	    <GlobalStyles />
+			  <Component {...pageProps} />
+		  </ThemeProvider>
+)
 }
 
 export default MyApp
---
+++ styles/themes.js
@@ -0,0 +1,9 @@
+export const lightTheme = {
+  body: '#FFFFFF',
+  text: '#363537',
+}
+
+export const darkTheme = {
+  body: '#191e2b',
+  text: '#FAFAFA',
+}
